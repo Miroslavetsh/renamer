638e8af5ad530459f39751c1782ffbac8ab7c805
Miroslavetsh <dfgshte@gmail.com>
2023-04-19 22:33:46 +0300

    Make a stable version

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const { readdirSync, rename } = require('fs')
 const { resolve } = require('path')
 
-const musicFolderPath = resolve(__dirname, '3rd Album')
+const musicFolderPath = resolve(__dirname, '[SPOTIFY-DOWNLOADER.COM] 3rd Album')
 
 const files = readdirSync(musicFolderPath)
 
 const prefixToDelete = '[SPOTIFY-DOWNLOADER.COM]'
 
-const newRename = (path) => {
-  path.includes(prefixToDelete) ?? rename(path, path.substring(prefixToDelete.length, path.length).trim(), )
-}
-
-newRename(musicFolderPath)
+// const newRename = (path) => {
+//   return rename(path, path.substring(prefixToDelete.length, path.length).trim())
+// }
 
 files.forEach((file) => {
   if (file.includes(prefixToDelete)) {
-    newRename(musicFolderPath + `/${file}`)
+    return rename(
+      musicFolderPath + `/${file}`,
+      musicFolderPath + `/${file.substring(prefixToDelete.length, file.length).trim()}`,
+      (err) => console.log(err),
+    )
   }
 })
